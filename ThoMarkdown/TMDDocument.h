2f880fc2d492900713f1f000517647d19d8b10c6
Thorsten Karrer <karrer@cs.rwth-aachen.de>
2012-02-19 13:33:16 +0100

    Initial Commit

---
+++ ThoMarkdown/TMDDocument.h
@@ -0,0 +1,13 @@
+//
+//  TMDDocument.h
+//  ThoMarkdown
+//
+//  Created by Thorsten Karrer on 19.2.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TMDDocument : NSDocument
+
+@end
